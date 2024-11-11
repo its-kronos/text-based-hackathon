e21d07415f8c8a3bfab3152a0a2378295796651e
krishik12 <krishik239@gmail.com>
2024-11-10 23:11:13 -0500

    updating level1.py with background
    Updating background by concatenating with 20 more "‾" characters. Will need to fix  after the clarification.

--- Utilities/level1.py
+++ Utilities/level1.py
@@ -1,11 +1,22 @@
+
 level = [
     [" "," "," "," "," "," "," "],
     ["T"," "," "," "," ","‾","‾"],
     ["‾","‾","‾","‾","‾"," "," "]
 ]
 
-turret_locations = []
+level_1 = [
+    ["T","_","_","_","_","_","_"],
+    ["_","_","_","_","_","_","_"],
+    ["_","_","_","_","_","_","_"]
+]
 
+i=0
+j=0
+index=0
+output = ""
+
+turret_locations = []
 character_location = [1,0] # Y,X, where Y goes from top
 
 def get_level():
@@ -14,3 +25,17 @@ def get_level():
         "turret_locations":turret_locations,
         "character_location":character_location
     }
+
+
+while index < len(level):
+    item = level[index]
+    index += 1 
+    for j in range(j, 20):
+        item.append("‾")
+    #reset the value of J to 0
+    j = 0
+
+
+for row in level:
+    output += " ".join(row) + "\n"
+print(output)
